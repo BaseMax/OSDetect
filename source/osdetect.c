fc67dbb9a7fe8cd64995b9353b13395a0ef68275
Max Base <MaxBaseCode@Gmail.Com>
2019-07-06 14:34:55 +0430

    Update osdetect.c

--- source/osdetect.c
+++ source/osdetect.c
@@ -1,1 +1,10 @@
-
+/**
+ *
+ * @Name : osdetect.c
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2019-07-02
+ * @Released under : https://github.com/BaseMax/OSDetect/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/OSDetect
+ *
+ **/
