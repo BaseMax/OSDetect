b8381e2e929e2b359baa9f1512f4948414e072ea
Max Base <MaxBaseCode@Gmail.Com>
2019-07-06 14:32:30 +0430

    Update osdetect.h

--- include/osdetect.h
+++ include/osdetect.h
@@ -8,3 +8,17 @@
  * @Repository : https://github.com/BaseMax/OSDetect
  *
  **/
+#ifndef OSDETECT
+	#define OSDETECT
+	#if defined(_WIN32) || defined(_WIN64)
+		#define OSDETECT_WINDOWS
+	#elif defined(__APPLE__) || defined(__MACH__)
+		#define OSDETECT_MAC
+	#elif defined(__linux__) || defined(linux) || defined(__linux)
+		#define OSDETECT_LINUX
+	#elif defined(unix) || defined(__unix) || defined(__unix__)
+		#define OSDETECT_UNIX
+	#elif defined(__FreeBSD__)
+		#define OSDETECT_BSD
+	#endif
+#endif
