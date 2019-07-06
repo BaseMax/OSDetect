fe4bbe4bbb67a3409978e8e7dcb708d2537e5d38
Max Base <MaxBaseCode@Gmail.Com>
2019-07-06 14:32:19 +0430

    Update osdetect.h

--- include/osdetect.h
+++ include/osdetect.h
@@ -1,1 +1,10 @@
-// https://github.com/BaseMax/OSDetect
+/**
+ *
+ * @Name : osdetect.h
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2019-07-02
+ * @Released under : https://github.com/BaseMax/OSDetect/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/OSDetect
+ *
+ **/
